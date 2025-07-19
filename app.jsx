650657c54634dd4ece6146b0e5a61c0b7a5349a6
ProtocoloApp <aeslabs23@gmail.com>
2025-07-19 20:15:07 -0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Visualizador de Imagens - Protocolo App</h1>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '16px', marginTop: '30px' }}>
+        <img src="/40unNXIQWsuu.jpg" alt="Imagem 1" width="250" />
+        <img src="/6KByN1ta6yHY.jpg" alt="Imagem 2" width="250" />
+        <img src="/6PCZZEudq2ah.jpg" alt="Imagem 3" width="250" />
+        <img src="/7crsnFfNYVg9.jpg" alt="Imagem 4" width="250" />
+        <img src="/87WcHrqqaLEP.jpg" alt="Imagem 5" width="250" />
+        <img src="/8TDySrCrzQJZ.jpg" alt="Imagem 6" width="250" />
+        <img src="/AmiaFFPg55yE.jpg" alt="Imagem 7" width="250" />
+        <img src="/CpPKwaUdvzih.jpg" alt="Imagem 8" width="250" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
